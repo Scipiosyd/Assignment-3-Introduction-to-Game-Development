32033fd0fe227897ca694ebd838a70914759bc0a
Christopher Dabrowski <physavinano@gmail.com>
2025-09-25 12:35:19 +1000

    feature movement

---
+++ Assets/Scripts/bunnycirclewalk.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bunnycirclewalk : MonoBehaviour
+{
+
+
+    public float speed = 1f;
+    public float moveverticaltime = 1f;
+    private float timeelapsed = 0f;
+    private float distpertile = 0.31775f;  //  1.271/4  1.271 is position x delta
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (timeelapsed < distpertile * 4)
+        {
+            float movedistance = speed * Time.deltaTime;
+            transform.position += Vector3.up * movedistance;
+            timeelapsed += Time.deltaTime;
+
+        }
+
+        if( timeelapsed > distpertile * 4 && timeelapsed < distpertile * 9)
+        {
+            transform.position += Vector3.right * (speed * Time.deltaTime);
+            timeelapsed += Time.deltaTime;
+        }
+
+        if(timeelapsed > distpertile * 9 && timeelapsed < distpertile * 13)
+        {
+            transform.position += Vector3.down * (speed * Time.deltaTime);
+            timeelapsed += Time.deltaTime;
+        }
+
+        if(timeelapsed > distpertile * 13 &&  timeelapsed < distpertile * 18)
+        {
+            transform.position += Vector3.left * (speed * Time.deltaTime);
+            timeelapsed += Time.deltaTime; 
+        }
+
+
+        if(timeelapsed >= distpertile * 18)
+        {
+            timeelapsed = 0f; 
+        }
+
+
+    }
+
+
+
+}
